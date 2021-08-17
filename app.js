fbbee74fc57c5d3e7daa3a31fa63964215f1d15c
patrycjastarzec <patryc.sc@gmail.com>
2021-08-17 21:13:47 +0200

    Add basic function and apply style

---
+++ app.js
@@ -0,0 +1,64 @@
+/*
+8 rows - 64 div - height/width 12.5%
+10 rows - 100 div - height/width 10%
+20 rows - 400 div - height/width 5%
+*/
+const button = document.getElementById('myBtn');
+const number = document.getElementsByName('number');
+
+const setValue = () => {
+  let number_value; 
+  for (let i = 0; i < number.length; i++){
+      if(number[i].checked){
+          number_value = number[i].value;
+      }
+  } 
+  return number_value;
+}
+
+const addDiv = () => {
+  let selectedNumber = setValue();
+  const mainDiv = document.querySelector('.grid-container');
+
+  for (let x = 0; x < selectedNumber; x++) {
+    let newDiv = document.createElement('div');
+    mainDiv.appendChild(newDiv);
+    newDiv.setAttribute("id", x);
+    newDiv.className = 'square';
+  
+    if (selectedNumber == 64){
+      newDiv.style.height = "12.5%";
+      newDiv.style.width = "12.5%";
+    } else if(selectedNumber == 100){
+      newDiv.style.height = "10%";
+      newDiv.style.width = "10%";
+    } else if(selectedNumber == 400){
+      newDiv.style.height = "5%";
+      newDiv.style.width = "5%";
+    }  
+  
+    function applyColor() {
+      newDiv.style.backgroundColor = "blue";    
+    }   
+    
+    newDiv.addEventListener("mouseover", applyColor);
+  } 
+}
+
+button.addEventListener('click', addDiv);
+
+
+
+  
+
+
+
+  
+
+
+
+
+
+
+
+
