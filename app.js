51e452bbef01f74fd10446d45756babafd6294ec
patrycjastarzec <patryc.sc@gmail.com>
2021-09-08 20:45:21 +0200

    Add button for removing grid

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-/*
-8 rows - 64 div - height/width 12.5%
-10 rows - 100 div - height/width 10%
-20 rows - 400 div - height/width 5%
-*/
-const button = document.getElementById('myBtn');
+const buttonCreate = document.getElementById('create-grid');
+const buttonRemove = document.getElementById('remove-grid');
 const number = document.getElementsByName('number');
+const mainDiv = document.querySelector('.grid-container');
 
 const setValue = () => {
   let number_value; 
@@ -18,23 +15,22 @@ const setValue = () => {
 
 const addDiv = () => {
   let selectedNumber = setValue();
-  const mainDiv = document.querySelector('.grid-container');
 
   for (let x = 0; x < selectedNumber; x++) {
     let newDiv = document.createElement('div');
     mainDiv.appendChild(newDiv);
-    newDiv.setAttribute("id", x);
+    newDiv.setAttribute('id', x);
     newDiv.className = 'square';
   
     if (selectedNumber == 64){
-      newDiv.style.height = "12.5%";
-      newDiv.style.width = "12.5%";
+      newDiv.style.height = '12.5%';
+      newDiv.style.width = '12.5%';
     } else if(selectedNumber == 100){
-      newDiv.style.height = "10%";
-      newDiv.style.width = "10%";
+      newDiv.style.height = '10%';
+      newDiv.style.width = '10%';
     } else if(selectedNumber == 400){
-      newDiv.style.height = "5%";
-      newDiv.style.width = "5%";
+      newDiv.style.height = '5%';
+      newDiv.style.width = '5%';
     }  
   
     function applyColor() {
@@ -42,18 +38,20 @@ const addDiv = () => {
       newDiv.style.backgroundColor = `rgb(${randomColorValue}%, ${randomColorValue}%, ${randomColorValue}%)`;    
     }   
     
-    newDiv.addEventListener("mouseover", applyColor);
+    newDiv.addEventListener('mouseover', applyColor);
 
-    button.disabled = true;
+    buttonCreate.disabled = true;
   } 
 }
 
-button.addEventListener('click', addDiv);
+buttonCreate.addEventListener('click', addDiv);
 
+const removeGrid = () => {
+  mainDiv.textContent = '';
+  buttonCreate.disabled = false;
+}
 
-
-  
-//background-color: rgb(255, 0, 0);
+buttonRemove.addEventListener('click', removeGrid);
 
 
   
