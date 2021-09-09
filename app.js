33bd0e1c41245cc2ebd1fe427df82c0a4f70c728
patrycjastarzec <patryc.sc@gmail.com>
2021-09-09 18:35:00 +0200

    Add function for selection of color palette

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const buttonCreate = document.getElementById('create-grid');
 const buttonRemove = document.getElementById('remove-grid');
 const number = document.getElementsByName('number');
+const color = document.getElementsByName('select-color');
 const mainDiv = document.querySelector('.grid-container');
 
 const setValue = () => {
   let number_value; 
   for (let i = 0; i < number.length; i++){
       if(number[i].checked){
-          number_value = number[i].value;
+        number_value = number[i].value;
       }
   } 
   return number_value;
 }
 
+const setColor = () => {
+  let color_value; 
+  for (let i = 0; i < color.length; i++){
+      if(color[i].checked){
+        color_value = color[i].value;
+      }
+  } 
+  return color_value;
+}
+
 const addDiv = () => {
   let selectedNumber = setValue();
+  let selectedColor = setColor();
 
   for (let x = 0; x < selectedNumber; x++) {
     let newDiv = document.createElement('div');
@@ -32,11 +44,18 @@ const addDiv = () => {
       newDiv.style.height = '5%';
       newDiv.style.width = '5%';
     }  
-  
+    
     function applyColor() {
-      const randomColorValue = Math.floor(Math.random() * 100);
-      newDiv.style.backgroundColor = `rgb(${randomColorValue}%, ${randomColorValue}%, ${randomColorValue}%)`;    
-    }   
+      if (selectedColor == 'color') {
+        const red = Math.floor(Math.random() * 100);
+        const green = Math.floor(Math.random() * 100);
+        const blue = Math.floor(Math.random() * 100);
+        newDiv.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
+      } else {
+        const randomColorValue = Math.floor(Math.random() * 100);
+        newDiv.style.backgroundColor = `rgb(${randomColorValue}%, ${randomColorValue}%, ${randomColorValue}%)`;  
+      } 
+    } 
     
     newDiv.addEventListener('mouseover', applyColor);
 
